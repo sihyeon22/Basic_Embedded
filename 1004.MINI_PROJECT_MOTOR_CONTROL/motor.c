e9b493682817d9a5cede8c5a656e6828f36ab17b
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-01-22 10:05:46 +0900

    Add files via upload

---
+++ 1004.MINI_PROJECT_MOTOR_CONTROL/motor.c
@@ -0,0 +1,29 @@
+#include "device_driver.h"
+
+void Motor_Init(void)
+{
+	Macro_Set_Bit(RCC->APB2ENR, 2); // PA activate
+	Macro_Write_Block(GPIOA->CRL, 0xFF, 0x22, 2*4);
+    Macro_Clear_Area(GPIOA->ODR, 0x3, 2);
+}
+
+
+#if 0
+
+// 아래는 해설 코드
+
+#define STOP        0x0
+#define FWD         0x1
+#define BWD         0x2
+#define MOT_DELAY   1
+
+void Motor_Run(int cmd) // 0: stop, 1: fwd, 2: bwd
+{
+    static int dir = 0; // 0: stop, 1: fwd, 2: bwd
+    static int mot[] = {STOP, FWD, BWD};
+
+    if ((dir * cmd) == 2) TIM2_Delay(MOT_DELAY);
+    Macro_Write_Block(GPIOA->ODR, 0x3, mot[cmd], 2);
+}
+
+#endif
---
+++ 1005.MINI_PROJECT_MOTOR_CONTROL2/main.c
@@ -0,0 +1,95 @@
+#include "device_driver.h"
+
+#if 0
+
+#define Turn_Right() 	(Macro_Write_Block(GPIOA->ODR, 0x3, 0x2, 2))
+#define Turn_Left() 	(Macro_Write_Block(GPIOA->ODR, 0x3, 0x1, 2))
+#define Motor_Stop()	(Macro_Clear_Area(GPIOA->ODR, 0x3, 2))
+
+static void Sys_Init(void)
+{
+	Uart_Init(115200);
+	Clock_Init();
+	LED_Init();
+	Key_Poll_Init();
+	Motor_Init();
+}
+
+void Main(void)
+{
+	Sys_Init();
+	Uart_Printf("Motor Test using Timer\n");
+	TIM4_Repeat(50);
+
+	for(;;)
+	{
+		char x = Uart1_Get_Pressed();
+		Uart_Printf("%c\n", x);
+		switch (x)
+		{
+			case 'F':
+				TIM2_Delay(1);
+				Turn_Right();
+				break;
+			case 'R':
+				TIM2_Delay(1);
+				Turn_Left();
+				break;
+			case 'S':
+				Motor_Stop();
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+#endif
+
+#if 1
+
+#define STOP    0x0
+#define FWD     0x1
+#define BWD     0x2
+
+static void Sys_Init(void)
+{
+	Uart_Init(115200);
+	Clock_Init();
+	LED_Init();
+	Key_Poll_Init();
+	Motor_Init();
+}
+
+void Control_Mode(int cmd)
+{
+	if (cmd == 0) TIM4_STOP();
+	else if (cmd == 1) TIM4_Repeat(500);
+	else if (cmd == 2) TIM4_Repeat(1000);
+}
+
+void Main(void)
+{
+	Sys_Init();
+	Uart1_Printf("Motor Test\n");
+
+	for(;;)
+	{
+		char x = Uart1_Get_Char(); // 'S' = 0, 'F' = 1, 'R' = 2
+		Uart1_Printf("%c\n", x);
+
+		if ((x == 'F') || (x == 'R') || (x == 'S'))
+		{
+			if (x == 'F') Motor_Run(FWD);
+			else if (x == 'R') Motor_Run(BWD);
+			else if (x == 'S') Motor_Run(STOP);
+		}
+		else if ((x == '0') || (x == '1') || (x == '2'))
+		{
+			Control_Mode((int)x);
+		}
+		else Uart1_Printf("Please retry.\n");
+	}
+}
+
+#endif
