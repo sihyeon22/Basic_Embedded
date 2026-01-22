1b52a5c2b8b2c8d6958bb4ac475b80b391f68c13
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-01-22 10:10:42 +0900

    Add files via upload

---
+++ 1104.UART_IRQ_LAB/main.c
@@ -0,0 +1,45 @@
+#include "device_driver.h"
+
+static void Sys_Init(void)
+{
+	Clock_Init();
+	LED_Init();
+	Uart_Init(115200);
+	Key_Poll_Init();
+
+	SCB->VTOR = 0x08003000;
+	SCB->SHCSR = 0;
+}
+
+extern volatile int Key_Value;
+
+void Main(void)
+{
+	Sys_Init();
+	Uart1_Printf("USART1 IRQ Test\n");
+
+	Key_ISR_Enable(1);
+
+	// DR 레지스터를 읽음으로써 RX buf pending clear
+	// volatile char x = USART1->DR
+	// (void)USART1->DR;
+	Macro_Clear_Bit(USART1->SR, 5);	// Pending Clear
+	Macro_Set_Bit(USART1->CR1, 5);	// USART1 RX interrupt enable
+
+	NVIC_ClearPendingIRQ(37);		// NVIC USART1 Pending clear
+	NVIC_EnableIRQ(37);				// NVIC USART1 interrupt enable
+
+	for(;;)
+	{
+		if(Key_Value)
+		{
+			Uart1_Printf("KEY=%d\n", Key_Value);
+			Key_Value = 0;
+		}
+
+		LED_Display(1);
+		TIM2_Delay(100);
+		LED_Display(2);
+		TIM2_Delay(100);
+	}
+}
